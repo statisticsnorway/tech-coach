48c47a501a98283f38fe84a9184995ba189c7102
Ola Vik Presterud <126689460+olaprest-ssb@users.noreply.github.com>
2023-10-17 14:46:17 +0200

    Final DuckDB example

--- src/parquet/queries.py
+++ src/parquet/queries.py
@@ -11,11 +11,9 @@ def create_table(name: str) -> None:
     data_dir = Path(__file__).parent / "dataset"
     file_path = str(data_dir / f"{name}.parquet")
     conn.execute(
-        f"""
-    CREATE OR REPLACE TABLE {name} AS
-        SELECT
-            * FROM '{file_path}'
-    """
+        f"""CREATE OR REPLACE TABLE {name} AS
+            SELECT * FROM '{file_path}'
+        """
     )
 
 
@@ -27,30 +25,11 @@ def create_tables():
     create_table("categories")
 
 
-def get_customer_orders(customer: str) -> None:
-    print("hello customer", customer)
-
+def get_customer_orders(customer: str) -> pd.DataFrame:
     create_tables()
-    result_customers = conn.execute(f"SELECT * FROM customers").df()
-    result_details = conn.execute(f"SELECT * FROM details").df()
-    result_orders = conn.execute(f"SELECT * FROM orders").df()
-
-    print("\ncustomers\n", result_customers)
-    print("\ndetails\n", result_details)
-    print("\norders\n", result_orders)
-
-    print(conn.execute("DESCRIBE orders").df())
-    #   print(conn.execute("DESCRIBE customers").df())
-    print(conn.execute("SELECT CustomerID FROM orders").df())
 
     query_str = f"""SELECT
         c.FirstName,
-        c.CustomerID,
-        o.CustomerID,
-        o.OrderID,
-        d.OrderID,
-        d.ProductID,
-        p.ProductID,
         p.ProductName
         FROM customers AS c
         JOIN orders AS o ON c.CustomerID = o.CustomerID
@@ -59,9 +38,9 @@ def get_customer_orders(customer: str) -> None:
         WHERE c.FirstName = '{customer}'
     """
 
-    result1 = conn.execute(query_str).df()
-
-    print(result1)
+    return conn.execute(query_str).df()
 
 
-get_customer_orders("Bob")
+if __name__ == "__main__":
+    result = get_customer_orders("Alice")
+    print(result)
