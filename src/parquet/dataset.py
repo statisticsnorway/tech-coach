7b02cf22853ad824b4c55261af205022ec62c23f
Ola Vik Presterud <126689460+olaprest-ssb@users.noreply.github.com>
2023-10-17 12:28:15 +0200

    Finished creating dataset

--- src/parquet/dataset.py
+++ src/parquet/dataset.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+
 import pandas as pd
 
 
@@ -7,6 +8,22 @@ def create_dataset() -> None:
     customers_df = pd.read_csv(data_dir / "customers.csv")
     print(customers_df.head())
     customers_df.to_parquet(data_dir / "customers.parquet")
+
+    orders_df = pd.read_csv(data_dir / "orders.csv")
+    print(orders_df.head())
+    orders_df.to_parquet(data_dir / "orders.parquet")
+
+    products_df = pd.read_csv(data_dir / "products.csv")
+    print(products_df.head())
+    products_df.to_parquet(data_dir / "products.parquet")
+
+    orderdetails_df = pd.read_csv(data_dir / "details.csv")
+    print(orderdetails_df.head())
+    orderdetails_df.to_parquet(data_dir / "details.parquet")
+
+    categories_df = pd.read_csv(data_dir / "categories.csv")
+    print(categories_df.head())
+    categories_df.to_parquet(data_dir / "categories.parquet")
 
 
 if __name__ == "__main__":
