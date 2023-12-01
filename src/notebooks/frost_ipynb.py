6361661d699265709fc748e84250b715749de9f1
Arne Sørli <81353974+arneso-ssb@users.noreply.github.com>
2023-12-01 14:05:05 +0100

    Get metrological data and store to new kildedata bucket

--- src/notebooks/frost_ipynb.py
+++ src/notebooks/frost_ipynb.py
@@ -34,7 +34,7 @@ to_date = "2010-12-31"
 # version = "1"
 
 # inn i configfil
-bucket = "gs://ssb-prod-tech-coach-data-kilde"
+bucket = "gs://ssb-tech-coach-data-kilde-prod"
 folder = "tip-tutorials/frost_data"
 
 now = datetime.now().replace(microsecond=0).isoformat().replace(":", "-")
@@ -99,7 +99,7 @@ df.head()
 # Write to parquet file
 parquet_file = path.replace(".json", ".parquet")
 print(f"Write parquet file: {parquet_file}")
-dp.write_pandas(df=df, gcs_path=parquet_file)
+# dp.write_pandas(df=df, gcs_path=parquet_file)
 
 # If you want to write to .parquet with plain Pandas, use the function below
 # df.to_parquet(parquet_file, storage_options=dp.pandas.get_storage_options())
