fdd0fce732ec9262a0d7662edf83b44f5a87e270
arneso-ssb <81353974+arneso-ssb@users.noreply.github.com>
2024-02-18 20:34:51 +0000

    Add documentation

--- src/notebooks/parquet-concatenation.py
+++ src/notebooks/parquet-concatenation.py
@@ -9,6 +9,24 @@
 
 # %% [markdown]
 # # Test på sammenslåing av parquet-filer i bøtter
+#
+# For transaksjonsbaserte statistikker kan det være en utfordring å jobbe med mange små
+# parquet-filer. Hver transaksjon lagres som en parquet-fil etterhvert som de kommer
+# inn. Og det kan ta minutter eller timer å lese inn alle småfilene som trengs. Derfor
+# trenger man å slå sammen alle småfilene til en, eller noen få, parquet-filer for å
+# kunne jobbe effektivt med dem.
+#
+# Koden her lager og lagrer mange små parquet-filer til en bøtte, og leser dem så inn
+# igjen, slår dem sammen til en fil, og leser den samlede filen. Tiden måles på alle
+# operasjoner, slik at man kan teste ytelse.
+#
+# Noen konkrete behov: Grunnboka har et par millioner transaksjoner som skal slås
+# sammen, og jakt og fangst rundt 200 000 filer.
+#
+# ## Hvordan teste med forskjellige størrelser?
+#
+# Endre på variaabelen `number_of_files` til ønskede antall filer og sett `write_files`
+# til `False` hvis du bare vil teste leseytelse.
 
 # %%
 import contextlib
@@ -25,6 +43,9 @@ bucket = "gs://ssb-prod-dapla-felles-data-delt"
 dir = f"/tech-coach/parquet-test/concat/dataset-{number_of_files}/"
 bucket_with_dir = bucket + dir
 print(bucket_with_dir)
+concat_file = (
+    bucket + f"/tech-coach/parquet-test/concat/dataset-{number_of_files}-concat.parquet"
+)
 
 
 # %%
@@ -69,4 +90,5 @@ with time_block(f"Writing {number_of_files} transaction files"):
         filename = f"{i:0{num_digits}}.parquet"  # pad the number with leading zeros
         df = create_transaction(i)
         dp.write_pandas(df, f"{bucket_with_dir}{filename}")
+
 # %%
